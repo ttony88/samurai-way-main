6e500b6d2019efc0b3c99d803acc23837c6f517e
ttony62 <1988anton62@gmail.com>
2024-01-24 22:31:42 +0300

    add Messages

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import './App.css';
-import { Header } from './Header/Header';
-import { NavBar } from './NavBar/NavBar';
+import { Header } from './components/Header/Header';
+import { NavBar } from './components/NavBar/NavBar';
 import { Profile } from './Profile/Profile';
+import { Messages } from './components/Messages/Messages';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <Header /> 
       <NavBar />
-      <Profile />
+      {/*<Profile />*/}
+      <Messages />
     </div>
   );
 }
---
+++ src/components/Messages/Messages.tsx
@@ -0,0 +1,14 @@
+import React, {FC}  from 'react'
+import style from './Messages.module.css'
+
+type MessagesProps = {
+    
+}
+export const Messages:FC<MessagesProps> = (props) => {
+
+    return(
+        <div>
+            Messages
+        </div>
+    )
+}
