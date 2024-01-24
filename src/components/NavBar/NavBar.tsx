cfb494b53107669a478587838d26eb3a253aea67
ttony62 <1988anton62@gmail.com>
2024-01-24 22:31:12 +0300

    add componens

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -6,8 +6,8 @@ export const NavBar:FC = () => {
     return(
         <nav className={style.navBar}>
            <ul className={style.menu}>
-                <li className={`${style.item} ${style.active}`}>Messages</li>
                 <li className={style.item}>Profile</li>
+                <li className={`${style.item} ${style.active}`}>Messages</li>
                 <li className={style.item}>Users</li>
                 <li className={style.item}>Setting</li>
            </ul> 
