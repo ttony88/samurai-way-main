8a0878c464a3fdd9c76142c162531d27e8e7c387
ttony62 <1988anton62@gmail.com>
2024-02-05 22:46:42 +0300

    add Messages

--- src/components/Messages/Messages.tsx
+++ src/components/Messages/Messages.tsx
@@ -1,5 +1,7 @@
 import React, {FC}  from 'react'
 import style from './Messages.module.css'
+import { Dialog } from './Dialog/Dialog'
+import { MessagesItem } from './MessagesItem/MessagesItem'
 
 type MessagesProps = {
     
@@ -7,8 +9,18 @@ type MessagesProps = {
 export const Messages:FC<MessagesProps> = (props) => {
 
     return(
-        <div>
-            Messages
+        <div className={style.dialogsPage}>
+            <div className={style.dialogs}>
+                <Dialog id='1' title='Anton' />
+                <Dialog id='2' title='Dasha' />
+                <Dialog id='3' title='Sava' />
+                <Dialog id='4' title='Rita' />
+            </div>
+            <div className={style.messages}>
+                <MessagesItem text='hi!' />
+                <MessagesItem text='hi, how are you?' />
+                <MessagesItem text='good' />
+            </div>
         </div>
     )
 }
