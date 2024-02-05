9947b6cbd1b9039443d8cfa4263a57833f2337fc
ttony62 <1988anton62@gmail.com>
2024-02-05 22:46:03 +0300

    add MessagesItem

---
+++ src/components/Messages/MessagesItem/MessagesItem.tsx
@@ -0,0 +1,14 @@
+import React, {FC}  from 'react'
+import style from './MessagesItem.module.css'
+
+type MessagesItemProps = {
+    text: string
+}
+export const MessagesItem:FC<MessagesItemProps> = (props) => {
+
+    return(
+        <div className={style.message}>
+            {props.text}
+        </div>
+    )
+}
