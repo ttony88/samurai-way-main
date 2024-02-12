1be17dea41b67b2b9ae5b818c9a64d3f59acaf61
ttony62 <1988anton62@gmail.com>
2024-02-12 22:20:22 +0300

    add form in Posts

--- src/Profile/Posts/Posts.tsx
+++ src/Profile/Posts/Posts.tsx
@@ -9,6 +9,11 @@ export const Posts:FC<PostsProps> = (props) => {
 
     return(
         <div className={style.wrapper}>
+            <form className={style.form} action="">
+                <textarea></textarea>
+                <button>add post</button>
+            </form>
+            
             <Post textPost="Hello how are you" countLike={4} />
             <Post textPost="I'm Anton" countLike={2} />
         </div>
